ac9b58de67fe3eaf8ddb1b3638c7829f6cdf936f
niebarbar <1984607221@qq.com>
2024-04-23 16:12:06 +0800

    build: init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// lib-flexible 用于设置 rem 基准值，也就是设置font-size。
+import 'amfe-flexible/index.min.js'
+
+
+createApp(App).mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
+import { resolve } from 'path'
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [VantResolver()],
+      }),
+      Components({
+        resolvers: [VantResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+  };
+})
